26ccbe0ca1f2318f99f7b62e7935544d3673c6a9
Andy <Andy>
2017-04-10 19:38:31 -0400

    I added methods to Arduino 2 class

--- Arduino_2/Arduino_2.ino
+++ Arduino_2/Arduino_2.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+/*  Source Code for Arduino 2
+ * This Arduino will be the reciever
+ */
 
+
+
+boolean authenticateUser(){
+  
+}
+
+void recieveStringData(){
+  
+}
+
+
+void updateStringData(){
+  
+}
+
+
+
+void sendStringData(){
+  
+}
+
+
+void setup() {
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 
 }
