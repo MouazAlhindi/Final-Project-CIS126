6867fb5e41ed51964861ce8140b36faeb31e519b
Mouaz Alhindi <mozalhindi@gmail.com>
2017-04-19 13:52:32 -0400

    fixed logic for dycription, ready for testing

--- Libaray/encode.h
+++ Libaray/encode.h
@@ -1,2 +1,2 @@
 extern "C" byte encode(byte, byte); // encode is the label in your assembly program to start
-extern "C" byte decrpt(byte, byte);
+extern "C" byte decrpt(byte, byte); // decode method
