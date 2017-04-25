6e3a7e7014af61f9afbc34d9562b714089175a95
Mouaz Alhindi <mozalhindi@gmail.com>
2017-04-25 18:19:35 -0400

    Added Sending Logic, bugs are present

--- Arduino_1/Arduino_1.ino
+++ Arduino_1/Arduino_1.ino
@@ -6,24 +6,55 @@
  * sending an encrypted message back
  */
 
+//Variables
+int reciverPin;
+int senderPin;
+char encryptedMessage[140];
+char decryptedMessage[140];
+
+
 //SEND METHODS
 
 //Method that initiates sending sequence
 void sendReadySignal(){
+  digitalWrite(senderPin, HIGH);
+  delay(500);
+  digitalWrite(senderPin, LOW);
+}
+
+//Method to send a letter serially
+void sendLetter(char x){
+  String let = String(encode(x), BIN);
+
+  if(let.length() == 8){
+    for(int i = 0; let.length(); i++){
+      if(String.equals("0")){
+        digitalWrite(senderPin, HIGH);
+        delay(500);
+      } else {
+        digitalWrite(senderPin, LOW);
+        delay(500);
+      }
+      
+    }
+  }
   
 }
 
 //Method that will send the given char list
 //via serial communication
-void sendStringData(char[] letters){
-  
+void sendStringData(char letters[]){
+  sendReadySignal();
+  for(char c: letters){
+    sendLetter(c);
+  }
 }
 
 //RECIEVE METHODS
 
 //Method that listens for correct ready signal
 void listenReadySignal(){
-  
+
 }
 
 //Methoda that will revieve in a serial communication pattern
@@ -35,6 +66,9 @@ void recieveStringData(){
 //Setup method
 void setup() {
 
+  pinMode(reciverPin, INPUT);
+  pinMode(senderPin, OUTPUT);
+  
   Serial.begin(9600);
 
 }
