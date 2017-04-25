630648c4fbc5858fc6f107b27356a6b495930e2c
Mouaz Alhindi <mozalhindi@gmail.com>
2017-04-25 18:50:29 -0400

    Sending Logic and encryption extraction logic added. NOTE: Finalize Serial Data Trnasfer

--- Arduino_1/Arduino_1.ino
+++ Arduino_1/Arduino_1.ino
@@ -1,3 +1,5 @@
+#include <encode.h>
+
 /*  Source Code for Arduino 1
  * This Arduino will be...
  * Sending an encrypted message
@@ -6,14 +8,35 @@
  * sending an encrypted message back
  */
 
+
+
 //Variables
 int reciverPin;
 int senderPin;
+int encryptionKey;
 char encryptedMessage[140];
 char decryptedMessage[140];
 
 
+
 //SEND METHODS
+
+void setEncryptionKey(char encrypt){
+  switch(encrypt){
+    case 'A':
+       encryptionKey = 1;
+       break;
+    case 'B':
+       encryptionKey = 2;
+       break;
+    case 'C':
+       encryptionKey = 3;
+       break;
+    case 'D':
+       encryptionKey = 4;
+       break;
+  }
+}
 
 //Method that initiates sending sequence
 void sendReadySignal(){
@@ -24,10 +47,10 @@ void sendReadySignal(){
 
 //Method to send a letter serially
 void sendLetter(char x){
-  String let = String(encode(x), BIN);
+  String let = String(encode(x, encryptionKey), BIN);
 
   if(let.length() == 8){
-    for(int i = 0; let.length(); i++){
+    for(int i = 0; i < let.length(); i++){
       if(let.equals("0")){
         digitalWrite(senderPin, HIGH);
         delay(500);
@@ -43,10 +66,10 @@ void sendLetter(char x){
 
 //Method that will send the given char list
 //via serial communication
-void sendStringData(char letters[]){
+void sendStringData(){
   sendReadySignal();
-  for(char c: letters){
-    sendLetter(c);
+  for(int x = 0; x < sizeof(encryptedMessage); x++){
+    sendLetter(encryptedMessage[x]);
   }
 }
 
--- Libaray/encode.h
+++
@@ -1,2 +0,0 @@
-extern "C" byte encode(byte, byte); // encode is the label in your assembly program to start
-extern "C" byte decrypt(byte, byte);
