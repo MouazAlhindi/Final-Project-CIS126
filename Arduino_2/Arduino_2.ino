78799bedc50ce98d892a12eb996e40b236d7ac07
Mouaz Alhindi <mozalhindi@gmail.com>
2017-04-26 16:38:46 -0400

    added user authentication logic

--- Arduino_2/Arduino_2.ino
+++ Arduino_2/Arduino_2.ino
@@ -12,6 +12,7 @@
 boolean valid = false;
 String userID[] = {"Alpha", "Bravo", "Charlie", "Delta", "Echo" };
 
+//VALIDATE USER FUNCTION
 void validateUser(String x){
   for(int i = 0; i < sizeof(userID); i++){
       if(userID[i].equals(x)){
@@ -20,24 +21,29 @@ void validateUser(String x){
   }
 }
 
+//SETUP FUNCTION
 void setup() {
-
   Wire.begin(8);
   Wire.onReceive(receiveEvent);
-  
   Serial.begin(9600);
 }
 
+//MAIN LOOP FUNCTION
 void loop() {
- delay(100);
-
+  delay(100);
+  if(Serial.available() > 0){
+      String uID = Serial.readString();
+      validateUser(uID);
+  }
 }
 
+//RECIEVE EVENT FUNCTION
+//IS CALLED EVERYTIME THE MASTER SENDER
+//TRYS TO COMMUNIFCATE WITH THE 
 void receiveEvent(int howMany) {
   if(valid == false){
       Serial.println("Incoming Message: WARNING! USER MUST BE VALIDATED");
   }
-  
   while (1 < Wire.available()) { // loop through all but the last
       char c = Wire.read(); // receive byte as a character
       Serial.print(c);         // print the character
